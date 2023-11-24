9ca707ed5cbc06166fc13ada62cf96874a57c605
arya <pratamawijaya549@gmail.com>
2023-11-24 20:34:16 +0700

    kalkulator sederhana

---
+++ latihan1.py
@@ -0,0 +1,37 @@
+awal = int(input("masukkan nilai awal = "))
+akhir = int(input("masukkan nilai akhir= "))
+
+def tambah(a,b):
+    return a + b
+
+def kurang(a,b):
+    return a - b
+
+def kali(a,b):
+    return a * b
+
+def bagi(a,b):
+    return a / b
+
+print("PROGRAM KALKULATOR SEDERHANA")
+print("1. PENJUMLAHAN")
+print("2. PENGURANGAN")
+print("3. PERKALIAN")
+print("4. PEMBAGIAN")
+
+pilihan=int(input("Masukkan pilihan = "))
+
+if pilihan == 1:
+    print(tambah(awal,akhir))
+if pilihan == 2:
+    print(kurang(awal,akhir))
+if pilihan == 3:
+    print(kali(awal,akhir))
+if pilihan == 4:
+    if akhir == 0:
+        print("tidak bisa dibagi")
+    else :
+        print(kurang(awal,akhir))
+   
+
+
