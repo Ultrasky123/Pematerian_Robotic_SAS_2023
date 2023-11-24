0b0bd1ea2fc7d690ce7cac4dc1f60ba7e58f08e5
musstt <musthofaanwari0110@gmail.com>
2023-11-24 20:45:24 +0700

    Kalkulator Sederhana 2.0

---
+++ fungsikalkulator.py
@@ -0,0 +1,11 @@
+def tambah(a, b):
+    return a + b
+def kurang(a, b):
+    return a - b
+def kali(a, b):
+    return a * b
+def bagi(a, b):
+    if b == 0:
+        return print("Angka tidak bisa dibagi 0")
+    else:
+        return a / b
--- latihan1.py
+++ latihan1.py
@@ -1,38 +1,33 @@
-awal = int(input("Masukkan nilai awal= "))
-akhir = int(input("Masukkan nilai akhir= "))
+import fungsikalkulator
+isLanjut = True
+while isLanjut:
+    print("Selamat Datang di Kalkulator Sederhana")
+    awal = int(input("Masukkan nilai awal= "))
+    akhir = int(input("Masukkan nilai akhir= "))
 
-print("Nilai awal", awal)
-print("Nilai akhir", akhir)
+    print("Nilai awal", awal)
+    print("Nilai akhir", akhir)
 
-def tambah(a, b):
-    return a + b
+    print("Kalkulator Sederhana")
+    print("1. Penjumlahan")
+    print("2. Pengurangan")
+    print("3. Perkalian")
+    print("4. Pembagian")
 
-def kurang(a, b):
-    return a - b
-
-def kali(a, b):
-    return a * b
-
-def bagi(a, b):
-    if b == 0:
-        return print("Angka tidak bisa dibagi 0")
+    pilihan = int(input("Masukkan Pilihan: "))
+    if pilihan == 1:
+        print(fungsikalkulator.tambah(awal, akhir)) 
+    elif pilihan == 2:
+        print(fungsikalkulator.kurang(awal, akhir))
+    elif pilihan == 3:
+        print(fungsikalkulator.kali(awal, akhir))
+    elif pilihan == 4:
+        print(fungsikalkulator.bagi(awal, akhir))
     else:
-        return a / b
-
-print("Kalkulator Sederhana")
-print("1. Penjumlahan")
-print("2. Pengurangan")
-print("3. Perkalian")
-print("4. Pembagian")
-
-pilihan = int(input("Masukkan Pilihan: "))
-if pilihan == 1:
-    print(tambah(awal, akhir)) 
-elif pilihan == 2:
-    print(kurang(awal, akhir))
-elif pilihan == 3:
-    print(kali(awal, akhir))
-elif pilihan == 4:
-    print(bagi(awal, akhir))
-else:
-    print("Masukkan pilihan yang benar")
+        print("Masukkan pilihan yang benar")
+    
+    isLanjut = input("Apakah anda ingin melanjutkan (Y/N)? ")
+    if isLanjut == 'N' or 'n':
+        isLanjut = False
+    else:
+        isLanjut == True
