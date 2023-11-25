cdbdd3d43c5b4cc8e812b98f5745a1a9fffd22b3
arya <pratamawijaya549@gmail.com>
2023-11-25 19:28:00 +0700

    latihan module

--- latihan1.py
+++ latihan1.py
@@ -1,19 +1,8 @@
-import kalkulator
+# module
+import calfunc
 
 awal = int(input("masukkan nilai awal = "))
 akhir = int(input("masukkan nilai akhir= "))
-
-def tambah(a,b):
-    return a + b
-
-def kurang(a,b):
-    return a - b
-
-def kali(a,b):
-    return a * b
-
-def bagi(a,b):
-    return a / b
 
 print("PROGRAM KALKULATOR SEDERHANA")
 print("1. PENJUMLAHAN")
@@ -24,16 +13,16 @@ print("4. PEMBAGIAN")
 pilihan=int(input("Masukkan pilihan = "))
 
 if pilihan == 1:
-    print(tambah(awal,akhir))
+    print(calfunc.tambah(awal,akhir))
 if pilihan == 2:
-    print(kurang(awal,akhir))
+    print(calfunc.kurang(awal,akhir))
 if pilihan == 3:
-    print(kali(awal,akhir))
+    print(calfunc.kali(awal,akhir))
 if pilihan == 4:
     if akhir == 0:
         print("tidak bisa dibagi")
     else :
-        print(kurang(awal,akhir))
+        print(calfunc.kurang(awal,akhir))
    
 
 
