947b561555d40c2a5b8c66f5d897389a45162579
adithrahman2 <adithyarhm2@gmail.com>
2023-11-25 09:20:00 +0700

    Kalkulator sederhana

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,29 +1,49 @@
+# Modul
 import CalFunc
 
-awal = int(input("Masukkan nilai awal = "))
-akhir = int(input("Masukkan nilai akhir = "))
+# Looping terus menerus
+while True:
+    # Input nilai
+    awal = int(input("Masukkan nilai awal = "))
+    akhir = int(input("Masukkan nilai akhir = "))
 
-print("Ini Nilai Awal = ", awal)
-print("Ini Nilai Awal = ", akhir)
+    # Menampilkan nilai
+    print("Ini Nilai Awal = ", awal)
+    print("Ini Nilai Awal = ", akhir)
 
-print("PROGRAM CALCULATOR SEDERHANA")
-print(" 1. Pertambahan")
-print(" 2. Pengurangan")
-print(" 3. Pengkalian")
-print(" 4. Pembagian")
-pilihan = int(input("Enter Pilihan : "))
+    # Program Kalkulator sederhana
+    print("PROGRAM CALCULATOR SEDERHANA")
+    print(" 1. Pertambahan")
+    print(" 2. Pengurangan")
+    print(" 3. Pengkalian")
+    print(" 4. Pembagian")
+    pilihan = int(input("Enter Pilihan : "))
 
-if pilihan == 1:
-    print(awal, " + ", akhir, " = ", CalFunc.tambah(awal, akhir))
-elif pilihan == 2:
-    print(awal, " - ", akhir, " = ", CalFunc.kurang(awal, akhir))
-elif pilihan == 3:
-    print(awal, " * ", akhir, " = ", CalFunc.kali(awal, akhir))
-elif pilihan == 4:
-    if akhir == 0:
-        print("Tidak bisa dibagi 0")
+    # Kodisi
+    if pilihan == 1:
+        print(awal, " + ", akhir, " = ", CalFunc.tambah(awal, akhir)) # Memanggil fungsi CalFunc
+    elif pilihan == 2:
+        print(awal, " - ", akhir, " = ", CalFunc.kurang(awal, akhir))
+    elif pilihan == 3:
+        print(awal, " * ", akhir, " = ", CalFunc.kali(awal, akhir))
+    elif pilihan == 4:
+        if akhir == 0: # Logic jika nilai awal dibagi dengan 0
+            print("Tidak bisa dibagi 0")
+        else:
+            print(awal, " / ", akhir, " = ", CalFunc.bagi(awal, akhir))
     else:
-        print(awal, " / ", akhir, " = ", CalFunc.bagi(awal, akhir))
-else:
-    print("Invalid Input")
+        print("Input tidak valid") # Bersambung ke line 48 jika user tidak input angka 1-4
 
+    # Loop agar user menginput y/n saja
+    while True:
+        c = input("Jalankan program lagi? (y/n): ")
+        # Menghentikan loop
+        if c.lower() == 'n': # c.lower() untuk merubah input menjadi lowercase
+            print("Program dimatikan. Terima kasih telah menggunakan!")
+            exit()
+        # Menjalankan loop
+        elif c.lower() == 'y': # c.lower() untuk merubah input menjadi lowercase
+            break
+        # Meminta user agar input y/n -> loop juga
+        else:
+            print("Input tidak valid. Harap masukkan y/n")
