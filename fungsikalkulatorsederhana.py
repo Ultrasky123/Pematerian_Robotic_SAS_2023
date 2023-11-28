b04d338c5c63210cb28a2a4e45e72ce0145d951f
ririn <ririnj45@gmail.com>
2023-11-28 22:35:37 +0700

    kalkulator

---
+++ fungsikalkulatorsederhana.py
@@ -0,0 +1,14 @@
+def tambah(a,b):
+    return a + b
+
+def perkalian(a,b):
+    return a * b
+
+def kurang(a,b):
+    return a - b 
+
+def bagi(a,b):
+    if b == 0:
+        return "Angka Tidak Bisa Dibagi"
+    else :
+        return a/b
--- latihan1.py
+++ latihan1.py
@@ -1,50 +1,39 @@
-awal = int(input ("Masukkan nilai awal = "))
-akhir = int (input ("Masukkan nilai akhir = "))
+import fungsikalkulatorsederhana
 
-print(awal)
-print(akhir)
+while True:
+    print ("PROGRAM KALKULATOR SEDERHANA")
+    awal = int (input("Masukkan nilai awal : "))
+    akhir = int (input("Masukkan nilai akhir : "))
 
-def tambah(a,b):
-    return a + b
-print(tambah(akhir, awal ))
+    print ("MENU PROGRAM KALKULATOR SEDERHANA")
+    print ("1. Penjumlahan")
+    print ("2. Pengurangan")
+    print ("3. perkalian")
+    print ("4. pembagian")
+    pilihan = int(input("Masukkan pilihan = "))
 
-def perkalian(a,b):
-    return a * b
-print (perkalian(akhir, awal))
+    if pilihan == 1:
+        print(fungsikalkulatorsederhana.tambah(awal,akhir))
 
-def kurang(a,b):
-    return a - b 
-print (kurang(akhir, awal))
+    if pilihan == 2:
+        print (fungsikalkulatorsederhana.kurang(akhir, awal))
 
-def bagi(a,b):
-    return a / b 
-print (bagi(akhir, awal))
+    elif pilihan == 3:
+        print (fungsikalkulatorsederhana.perkalian(akhir, awal))
 
-print ("PROGRAM KALKULATOR SEDERHANA")
-print ("1. Penjumlahan")
-print ("2. Pengurangan")
-print ("3. perkalian")
-print ("4. pembagian")
-pilihan = int(input("Masukkan pilihan = "))
-
-if pilihan == 1:
-    print(tambah(awal,akhir))
-
-if pilihan == 2:
-    print (kurang(akhir, awal))
-
-elif pilihan == 3:
-    print (perkalian(akhir, awal))
-
-elif pilihan == 4:
-    
-    if akhir == 0:
-        print ("angka tidak bisa dibagi")
+    elif pilihan == 4:
+        
+        if akhir == 0:
+            print ("angka tidak bisa dibagi")
+        else:
+            print (fungsikalkulatorsederhana.bagi(akhir, awal))
     else:
-        print (bagi(akhir, awal))
-else:
-    print ("pilihan tidak valid")
+        print ("pilihan tidak valid")
 
+    hitunglagi = input("Apakah Anda Ingin Menggunakan Kalkulator lagi (y/n)?")
+    if hitunglagi.lower() == 'n':
+        print ("Program Selesai")
+        break
     
 
 
