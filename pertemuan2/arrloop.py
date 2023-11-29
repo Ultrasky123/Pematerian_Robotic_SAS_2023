5311046a50fb79259c00f92f9569beb6bbcab55d
titisyads <titisyadewi@gmail.com>
2023-11-29 19:34:38 +0700

    for loop

---
+++ pertemuan2/arrloop.py
@@ -0,0 +1,32 @@
+#1..
+
+
+def main():
+    size = int(input())
+    for i in range (0, size +1):
+        for j in range(0, i):
+            print('*' , end="")
+            print('')
+
+if __name__ == '__main__':
+    main()
+
+#2..
+
+def main():
+    size = int(input())
+    i = 1
+# untuk setiap i sebanyak size
+#selama kondisi i < size adalah TRUE 
+
+    while i <= size:
+        j = size
+        while j >= i:
+            print('*', end='')
+            j -= 1
+        print('')
+        i += 1
+
+#untuk memastikan bahwa name dari program ini adalah__main__
+if __name__ == '__main__':
+    main()
