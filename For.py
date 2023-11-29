46b4b988b726a1e556666bae96f6c7ac714080cc
Irvan <irvanromadon@gmail.com>
2023-11-29 19:39:52 +0700

    This is new

--- For.py
+++ For.py
@@ -2,8 +2,8 @@ def main():
     size = int(input())
 
    # For Loop
-    for i in range(0, size):
-        for j in range(0, i):
+    for i in range(0, size + 1):
+        for j in range(0, i + 1):
             print('*', end='')
         print('')
        
