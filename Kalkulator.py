049d62347b7d24b9cb5a3ea91124d7effa31a07b
Irvan <irvanromadon@gmail.com>
2023-11-29 19:31:30 +0700

    This is my commit(ment)

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,9 +1,3 @@
-awal = int(input("Masukkan Nilai awal = "))
-akhir = int(input("Masukkan nilai akhir = "))
-
-print(awal)
-print(akhir)
-
 def tambah(a, b):
     return a + b
 
@@ -14,29 +8,33 @@ def kali(a, b):
     return a * b
 
 def bagi(a, b):
+    if b == 0:
+        print("Tidak bisa dibagi dengan 0")
+        return None
     return a / b
 
-print ("PROGRAM KALKULATOR SEDERHANA")
-print ("Pilih program : ")
-print ("1. Tambah")
-print ("2. Kurang")
-print ("3. Kali")
-print ("4. Bagi")
+awal = int(input("Masukkan Nilai awal = "))
+akhir = int(input("Masukkan nilai akhir = "))
 
-pilihan = int(input("masukkan pilihan = "))
+print(awal)
+print(akhir)
+
+print("PROGRAM KALKULATOR SEDERHANA")
+print("Pilih program : ")
+print("1. Tambah")
+print("2. Kurang")
+print("3. Kali")
+print("4. Bagi")
+
+pilihan = int(input("Masukkan pilihan = "))
 
 if pilihan == 1:
     print(tambah(awal, akhir))
-
 elif pilihan == 2:
     print(kurang(awal, akhir))
-
 elif pilihan == 3:
     print(kali(awal, akhir))
-
 elif pilihan == 4:
-    print(bagi(awal, akhir))
-    if akhir or awal == 0:
-        print("Tidak bisa dibagi 0")
-    else:
-        print(bagi(awal, akhir))
+    hasil = bagi(awal, akhir)
+    if hasil is not None:
+        print(hasil)
