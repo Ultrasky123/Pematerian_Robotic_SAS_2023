921fb78a86fa449e248ada50f8046b8a9fc4fa7e
ghifarimramadhan <ghifarimmmr@gmail.com>
2023-11-29 19:28:57 +0700

    ieu pertemuan anu tgl 29

---
+++ pertemuan3anuenggal.py
@@ -0,0 +1,29 @@
+'''def main():
+
+    size = int(input("* = " ))
+
+    for i in range(0, size):
+        for j in range(0, i):
+            print ('*', end='')
+        print()
+
+if __name__== '__main__':
+    main()'''
+
+''''''''''''''''''''''''''''''''
+
+'''def main():
+
+    size = int(input(" = "))
+
+    i = 1
+    while i <= size+1:
+        j = size
+        while j > i:
+            print('*', end='')
+            j -= 1
+        print('')
+        i += 1
+
+if __name__== '__main__':
+    main()'''
