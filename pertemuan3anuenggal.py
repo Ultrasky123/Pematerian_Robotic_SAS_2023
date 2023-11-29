7ff93956a484f877317b2ae187a43df53dcb86c0
ghifarimramadhan <ghifarimmmr@gmail.com>
2023-11-29 19:59:19 +0700

    asdfghjkl

--- pertemuan3anuenggal.py
+++ pertemuan3anuenggal.py
@@ -27,3 +27,29 @@ if __name__== '__main__':
 
 if __name__== '__main__':
     main()'''
+
+def main():
+
+    row = int(input())
+
+    for i in range(1, row+1):
+        for j in range(1,row-i+1):
+            print(" ", end="")
+        for j in range(1, 2*i):
+            if j==1 or j==2*i-1:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
+
+    for i in range(row-1,0, -1):
+        for j in range(1,row-i+1):
+            print(" ", end="")
+        for j in range(1, 2*i):
+            if j==1 or j==2*i-1:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
+if __name__== '__main__':
+    main()
