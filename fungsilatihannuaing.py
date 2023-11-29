789163982ba2db79e17aa532f1400010d11215e6
ghifarimramadhan <ghifarimmmr@gmail.com>
2023-11-29 10:59:55 +0700

    nu enggal

---
+++ fungsilatihannuaing.py
@@ -0,0 +1,11 @@
+def penambahan(a,b) :
+    return a + b
+
+def pengkurangan(a,b) :
+    return a - b
+
+def pengkalian(a,b) :
+    return a * b
+
+def pembagian(a,b) :
+    return a / b
--- latihannuaing.py
+++ latihannuaing.py
@@ -1,23 +1,11 @@
+import fungsilatihannuaing
+
+print("Kalkulator Sederhana")
 awal = int(input("Masukan Nilai Awal = "))
 akhir = int(input("Masukan Nilai Akhir = "))
 
 print( "Nilai Awal", awal)
 print("Nilai Akhir", akhir)
-
-print(" ")
-
-def penambahan(a,b) :
-    return a + b
-
-def pengkurangan(a,b) :
-    return a - b
-
-def pengkalian(a,b) :
-    return a * b
-
-def pembagian(a,b) :
-    return a / b
-
 print("Beberapa Pilihan Operasi")
 print("1. penambahan")
 print("2. pengkurangan")
@@ -27,16 +15,15 @@ print("4. pembagian")
 pilihan = int(input("Input Operasi = "))
 
 if pilihan == 1:
-    print ("Hasil Penambahan = ", penambahan(awal,akhir))
-
+    print (fungsilatihannuaing.penambahan(awal,akhir))
 elif pilihan == 2:
-    print ("Hasil pengkurangan = ", pengkurangan(awal,akhir))
+    print (fungsilatihannuaing.pengkurangan(awal,akhir))
 elif pilihan == 3:
-    print ("Hasil Pengkalian = ", pengkalian(awal,akhir))
+    print (fungsilatihannuaing.pengkalian(awal,akhir))
 elif pilihan == 4:
     if akhir == 0:
         print("Tidak bisa dipembagian 0")
     else:
-        print ("Hasil Penambahan = ", pembagian(awal,akhir))
+        print (fungsilatihannuaing.pembagian(awal,akhir))
 else:
     print ("Masukan pilihan operasi yang benar")
