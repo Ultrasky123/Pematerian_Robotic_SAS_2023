b7b2c4a843e44864a9f91eb0a572822367b7b2de
RIco <arkmage76@gmail.com>
2023-11-29 20:42:37 +0700

    latihan day 3

--- day3.py
+++ day3.py
@@ -12,16 +12,37 @@ def main():
     #        print('*', end=' ')
     #    print('')
 
-    x = 12
-    i = 1
+    #x = 12
+    #i = 1
 
-    while i<=x:
-        j = 1
-        while j < i:
-            print('*', end = '')
-            j+=1
-        print('')
-        i+=1
+    #while i<=x:
+    #    j = 1
+    #    while j < i:
+    #        print('*', end = '')
+    #        j+=1
+    #    print('')
+    #    i+=1
+    
+
+    #   *   > 0
+    #  * *  > 1
+    # *   * > 3
+    #top part
+
+    #*     * middle part > 5
+
+    #bottom part
+    # *   * > 3
+    #  * *  > 1
+    #   *   > 0
+    
+    size = int(input("Enter the size: "))
+
+    for i in range(1, size + 1):
+        print(' ' * (size - i) + "*" + ' ' * (2 * i - 3) + ("*" if i > 1 else ""))
+
+    for i in range(size - 1, 0, -1):
+        print(' ' * (size - i) + "*" + ' ' * (2 * i - 3) + ("*" if i > 1 else ""))
 
 if __name__ == '__main__':
     main()
