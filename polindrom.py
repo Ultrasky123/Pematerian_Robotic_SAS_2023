a69dce051b3874cee259719dfa24fd9ca2f3deac
musstt <musthofaanwari0110@gmail.com>
2023-11-30 20:33:05 +0700

    update dikit

--- polindrom.py
+++ polindrom.py
@@ -1,16 +1,18 @@
+import os
 def kataKebalik(kata): # Paling Simpel
     return kata[::-1]
 
 def kataKebalik2(kata): # Pakai for loop
     str = "" # String Kosong
     for i in kata:
-        print("i = ", i)
+        # print("i = ", i)
         str = i + str 
-        print ("str = ", str)
+        # print ("str = ", str)
     return str
 
 def main():
     kata = str(input("Masukkan kata: "))
+    kata = kata.lower()
     if kata == kataKebalik2(kata=kata):
         print("True") 
     else:
@@ -21,6 +23,7 @@ if __name__ == '__main__':
     while True:
         lanjut = str(input("Apakah anda ingin memasukkan kata lagi (y/n)? "))
         if lanjut.lower() == 'y':
+            os.system('clear')
             main()
         else:
             print("Program Selesai")
