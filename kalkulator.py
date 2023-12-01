a26ae4b708ab163d2de2e323001e5f7951c36e15
arya <pratamawijaya549@gmail.com>
2023-12-01 20:12:37 +0700

    kalkulator sederhana

--- kalkulator.py
+++ kalkulator.py
@@ -1,71 +1,58 @@
+import calfunc
+
 print()
 print("===== PROGRAM KALKULATOR SEDERHANA =====")
 print()
 
-awal = int(input("Masukkan nilai awal : "))
-akhir = int(input("Masukkan nilai akhir : "))
+# loop tampilan
+while True :
+    awal = int(input("Masukkan nilai awal : "))
+    akhir = int(input("Masukkan nilai akhir : "))
+    print()
+    print("MASUKKAN PILIHAN")
+    print("1. Penjumlahan")
+    print("2. Pegurangan")
+    print("3. Perkalian")
+    print("4. Pembagian")
+    print("5. Exit")
 
-print()
+    pilihan = int(input("Masukkan pilihan : "))
 
-print("MASUKKAN PILIHAN")
-print("1. Penjumlahan")
-print("2. Pegurangan")
-print("3. Perkalian")
-print("4. Pembagian")
-print("5. Exit")
-
-pilihan = int(input("Masukkan pilihan : "))
-
-def tambah(a,b):
-    return  a + b
-
-def kurang(a,b):
-    return  a - b
-
-def kali(a,b):
-    return  a * b
-
-def bagi(a,b):
-    return  a / b
-
-while pilihan != 5 :
+    # kondisi
     if pilihan == 1:
-        print("Hasil :", tambah(awal,akhir))
+        print("Hasil :",calfunc.tambah(awal,akhir))
         print("============")
-        pilihan = int(input("Masukkan pilihan : "))
-        if pilihan == 5 :
-            break 
     elif pilihan == 2:
-        print("Hasil :",kurang(awal, akhir))
+        print("Hasil :",calfunc.kurang(awal, akhir))
         print("============")
-        pilihan = int(input("Masukkan pilihan : "))
-        if pilihan == 5 :
-            break
     elif pilihan == 3:
-        print("Hasil :",kali(awal, akhir))
+        print("Hasil :",calfunc.kali(awal, akhir))
         print("============")
-        pilihan = int(input("Masukkan pilihan : "))
-        if pilihan == 5 :
-            break
     elif pilihan == 4:
         if akhir == 0:
             print("Angka tidak bisa dibagi dengan 0")
             print("============")
-            pilihan = int(input("Masukkan pilihan : "))
-            if pilihan == 5 :
-                break
         else: 
-            print("Hasil :",bagi(awal, akhir))
+            print("Hasil :",calfunc.bagi(awal, akhir))
             print("============")
-            pilihan = int(input("Masukkan pilihan : "))
-            if pilihan == 5 :
-                break
+    elif pilihan== 5:
+        print("SELESAI")
+        break
     else:
         print("Input invalid")
         print("============") 
-        pilihan = int(input("Masukkan pilihan : "))
-    if pilihan == 5:
-        break
-    awal = int(input("Masukkan nilai awal : "))
-    akhir = int(input("Masukkan nilai akhir : "))
-    print("============")
+    
+   # Loop agar user menginput y/n saja
+    while True:
+        c = input("Jalankan program lagi? (y/n): ")
+        # Menghentikan loop
+        if c.lower() == 'n': # c.lower() untuk merubah input menjadi lowercase
+            print("Program dimatikan. Terima kasih telah menggunakan!")
+            exit()
+        # Menjalankan loop
+        elif c.lower() == 'y': # c.lower() untuk merubah input menjadi lowercase
+            break
+        # Meminta user agar input y/n -> loop juga
+        else:
+            print("Input tidak valid. Harap masukkan y/n")
+        
