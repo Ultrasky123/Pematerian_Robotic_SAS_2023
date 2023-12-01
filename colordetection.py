6d472dc1f6267d49fbd357d72a964142b56a6718
Rico <arkmage76@gmail.com>
2023-12-01 20:23:19 +0700

    color correction

--- colordetection.py
+++ colordetection.py
@@ -51,7 +51,8 @@ def main(capture):
 
             x, y, w, h = cv2.boundingRect(largest_contour)
 
-            cv2.rectangle(frame, (x, y), (w + x, h + y), (0, 0, 255), 2)
+            cv2.putText(frame, 'BLUE', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            cv2.rectangle(frame, (x, y), (w + x, h + y), (255, 0, 0), 2)
             
         cv2.imshow('Thresh', thresh)
         cv2.imshow('Frame', frame)
