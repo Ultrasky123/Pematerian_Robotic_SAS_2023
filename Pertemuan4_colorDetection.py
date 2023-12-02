866890d23b7cd1db5085ef56602d4d1975b83e2d
Aswangga <aswanggasadu@gmail.com>
2023-12-02 23:07:52 +0700

    Tugas Deteksi Warna

--- Pertemuan4_colorDetection.py
+++ Pertemuan4_colorDetection.py
@@ -3,6 +3,7 @@ import cv2
 def callback(_) :
     pass
 
+
 def init_trackbars():
     #HSV 
     # Bikin Window
@@ -11,9 +12,18 @@ def init_trackbars():
     cv2.createTrackbar('LS', 'HSV Trackbar', 0, 255, callback)
     cv2.createTrackbar('LV', 'HSV Trackbar', 0, 255, callback)
 
-    cv2.createTrackbar('UH', 'HSV Trackbar', 255, 255, callback)
-    cv2.createTrackbar('US', 'HSV Trackbar', 255, 255, callback)
-    cv2.createTrackbar('UV', 'HSV Trackbar', 255, 255, callback)
+    cv2.createTrackbar('UH', 'HSV Trackbar', 0, 255, callback)
+    cv2.createTrackbar('US', 'HSV Trackbar', 0, 255, callback)
+    cv2.createTrackbar('UV', 'HSV Trackbar', 0, 255, callback)
+
+    cv2.namedWindow('HSV Trackbar2')
+    cv2.createTrackbar('LH2', 'HSV Trackbar', 0, 255, callback)
+    cv2.createTrackbar('LS2', 'HSV Trackbar', 0, 255, callback)
+    cv2.createTrackbar('LV2', 'HSV Trackbar', 0, 255, callback)
+
+    cv2.createTrackbar('UH2', 'HSV Trackbar', 0, 255, callback)
+    cv2.createTrackbar('US2', 'HSV Trackbar', 0, 255, callback)
+    cv2.createTrackbar('UV2', 'HSV Trackbar', 0, 255, callback)
 
 def get_lower_hsv() :
     lower_hue= cv2.getTrackbarPos('LH', 'HSV Trackbar')
@@ -22,12 +32,29 @@ def get_lower_hsv() :
 
     return (lower_hue, lower_sat, lower_val)
 
+
+def get_lower_hsv2() :
+    lower_hue2= cv2.getTrackbarPos('LH2', 'HSV Trackbar')
+    lower_sat2= cv2.getTrackbarPos('LS2', 'HSV Trackbar')
+    lower_val2= cv2.getTrackbarPos('LV2', 'HSV Trackbar')
+
+    return (lower_hue2, lower_sat2, lower_val2)
+
 def get_upper_hsv() :
     upper_hue= cv2.getTrackbarPos('UH', 'HSV Trackbar')
     upper_sat= cv2.getTrackbarPos('US', 'HSV Trackbar')
     upper_val= cv2.getTrackbarPos('UV', 'HSV Trackbar')
 
+
     return (upper_hue, upper_sat, upper_val)
+
+def get_upper_hsv2() :
+
+    upper_hue2= cv2.getTrackbarPos('UH2', 'HSV Trackbar')
+    upper_sat2= cv2.getTrackbarPos('US2', 'HSV Trackbar')
+    upper_val2= cv2.getTrackbarPos('UV2', 'HSV Trackbar')
+
+    return (upper_hue2, upper_sat2, upper_val2)
 
 
 
@@ -42,33 +69,50 @@ def main(capture) :
         lowerHSV = get_lower_hsv()
         upperHSV = get_upper_hsv()
 
+        lowerHSV2 = get_lower_hsv2()
+        upperHSV2 = get_upper_hsv2()
         # print(lowerHSV)
         # print(upperHSV)
 
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         thresh = cv2.inRange(hsv, lowerHSV, upperHSV)
-        
+        thresh2 = cv2.inRange(hsv, lowerHSV2, upperHSV2)
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
         thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
         contour, _= cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        thresh2 = cv2.morphologyEx(thresh2, cv2.MORPH_OPEN, kernel)
+        contour2, _= cv2.findContours(thresh2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
         if contour :
             largest_contours = max(contour, key=cv2.contourArea)
+            
 
 
         # x dan y adalah koordinat/w dan h adalah ukuran
             x, y, w, h = cv2.boundingRect(largest_contours)
+            
 
             cv2.putText(frame, 'BLUE', (x, y+50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 3)
-            cv2.putText(frame, 'RED', (x, y+50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+            
 
             cv2.rectangle(frame, (x, y), (w + x, h + y), (255, 0,0), 2)
-            cv2.rectangle(frame, (x, y), (w + x, h + y), (0, 255,0), 2)
 
-        print(x, y, w, h)
+        if contour2 : 
+
+            largest_contours2 = max(contour2, key=cv2.contourArea)
+
+            a, b, c, d= cv2.boundingRect(largest_contours2)
+
+            cv2.putText(frame, 'GREEN', (a, b+50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+            cv2.rectangle(frame, (a, b), (c + a, d + b), (0, 255,0), 2)
+
+        # print(x, y, w, h)
 
         cv2.imshow('kernel', kernel)
         cv2.imshow('thresh', thresh)
+        cv2.imshow('thresh2', thresh2)
+
         cv2.imshow('HSV', hsv)
         cv2.imshow('Frame', frame)
 
@@ -81,6 +125,7 @@ def main(capture) :
 
 if __name__ == '__main__' :
     init_trackbars()
+
 
     camera = cv2.VideoCapture(0)
     main(camera)
