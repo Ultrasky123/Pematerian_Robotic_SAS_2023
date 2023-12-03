9a21eed8afb995660f5364a0e4fe8a8edd859c70
arya <pratamawijaya549@gmail.com>
2023-12-03 16:29:59 +0700

    tugas 4(1) deteksi warna hijau dan biru

--- color-detection.py
+++ color-detection.py
@@ -1,12 +1,92 @@
+# basis opencv
 import cv2
+import numpy as np
 
-cap = cv2.VideoCapture(0)
-while True:
-    ret,frame = cap.read()
-    cv2.imshow('frame',frame)
+def callback(_):
+    pass
 
-    if cv2.waitKey(1)& 0xFF == ord('q'):
-        break
+def init_trackbars():
+    # HSV(HUE,SATURATION,VALUE)
+    cv2.namedWindow('HSV Trackbars')
+    cv2.createTrackbar('LH','HSV Trackbars',0,255, callback)
+    cv2.createTrackbar('LS','HSV Trackbars',0,255, callback)
+    cv2.createTrackbar('LV','HSV Trackbars',0,255, callback)
+    cv2.createTrackbar('UH','HSV Trackbars',255,255, callback)
+    cv2.createTrackbar('US','HSV Trackbars',255,255, callback)
+    cv2.createTrackbar('UV','HSV Trackbars',255,255, callback)
 
-cap.release()
-cv2.destroyAllWindows()
+def get_lower_hsv():
+    lower_hue = cv2.getTrackbarPos('LH','HSV Trackbars')
+    lower_sat = cv2.getTrackbarPos('LS','HSV Trackbars')
+    lower_val = cv2.getTrackbarPos('LV','HSV Trackbars')
+    return(lower_hue,lower_sat,lower_val)
+
+def get_upper_hsv():
+    upper_hue = cv2.getTrackbarPos('UH','HSV Trackbars')
+    upper_sat = cv2.getTrackbarPos('US','HSV Trackbars')
+    upper_val = cv2.getTrackbarPos('UV','HSV Trackbars')
+    return(upper_hue,upper_sat,upper_val)
+
+lower_blue = np.array([110,50,50])
+upper_blue = np.array([130,255,255])
+lower_green = np.array([45,100,20])
+upper_green = np.array([75,255,255])
+
+def main(capture):
+    while True:
+        # print(capture.read())
+        # baca kamera
+        ret, frame = capture.read()
+        
+        # simpan nilai HSV
+        lower_hsv = get_lower_hsv()
+        upper_hsv = get_upper_hsv()
+        # print(lower_hsv)
+        # print(upper_hsv)
+
+        # Konvert warna dari BGR ke HSV
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        thresh = cv2.inRange(hsv,lower_hsv, upper_hsv)
+
+        # mengurangi noise hasil dari filter warna
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+        thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+
+        # generate kontur objek
+        image =cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        mask1 = cv2.inRange(image,lower_green,upper_green)
+        mask2 = cv2.inRange(image,lower_blue,upper_blue)
+        contours, _ = cv2.findContours(mask1,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours1, _ = cv2.findContours(mask2,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # ketika ada warna hijau
+        if len(contours)!= 0:
+            for contour in contours:
+                if cv2.contourArea(contour)>500:
+                    x,y,w,h = cv2.boundingRect(contour)
+                    cv2.rectangle(frame,(x,y),(w+x,h+y), (0,255,0),2)
+                    cv2.putText(frame,'GREEN',(x,y),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),2)
+        # ketika ada warna biru
+        if len(contours1)!= 0:
+            for contour in contours1:
+                if cv2.contourArea(contour)>500:
+                    x,y,w,h = cv2.boundingRect(contour)
+                    cv2.rectangle(frame,(x,y),(w+x,h+y), (255,0,0),2)
+                    cv2.putText(frame,'BLUE',(x,y),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0),2)
+
+        # tampilkan frame
+        cv2.imshow('mask',mask1+mask2)
+        cv2.imshow('Frame',frame)
+
+        # kondisi untuk end while loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # lepas cam & destroy windows
+    frame.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    init_trackbars()
+    camera = cv2.VideoCapture(0)
+    main(camera)
