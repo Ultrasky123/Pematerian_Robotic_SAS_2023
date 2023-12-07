ac7fdc181d42087191db09927222d4f36e40a3dd
arya <pratamawijaya549@gmail.com>
2023-12-07 19:23:18 +0700

    deteksi-object

--- materi-5.py
+++ materi-5.py
@@ -21,20 +21,43 @@ def main(capture):
         gray = cv2.equalizeHist(gray)
 
         # deteksi haar cascade
-        upperbody = upperbody_cascade.detectMultiScale(gray)
+        upperBody = upperbody_cascade.detectMultiScale(gray)
         faces = face_cascade.detectMultiScale(gray)
 
-        for (x3,y3,h3,w3) in upperbody :
-        # untuk setiap wajah yang terdeteksi
-            for (x,y,w,h) in faces :
+        for (x3, y3, w3, h3) in upperBody :
+
+        # Untuk setiap wajah yang terdeteksi
+            for (x, y, w, h) in faces :
+
                 smile = smile_cascade.detectMultiScale(gray)
-                for (a,b,c,d) in smile :
-                    if len(smile) >= 30:
-                        cv2.putText(frame,f"smile{len(smile)}",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-                        cv2.rectangle(frame,(x,y),(w+x,h+y), (0,255,0),2)
-                    else:
-                        cv2.putText(frame,f"no smile{len(smile)}",(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
-                        cv2.rectangle(frame,(x,y),(w+x,h+y), (0,0,255),2)
+
+                for (a, b, c, d) in smile :
+                # Simpan koordinat titik tengah wajah
+
+                    if len(smile) > 30 :
+                        # center = ((x + w //2, y + h //2))
+                        # Tampilkan lingkaran
+                        cv2.putText(frame, f'Senyum {len(smile)}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                        cv2.rectangle(frame, (x, y), (w + x, h + y), (0, 255,0), 2)
+                        # Tentukan wilayah awajah yang terdeteksi
+                        # face_roi = gray[y:y+h, x:x+w]
+                        # # print(face_roi)
+                        # eyes = eye_cascade.detectMultiScale(face_roi)
+                        # Untuk Setiap Mata Yang terdeteksi
+                        # for (x2, y2, w2, h2) in eyes :
+                        #     # Tentukan koordinat tengah mata
+                        #     eye_center = (x+x2+w2//2, y + y2 + h2//2)
+                        #     # Simpan nilai jari2 dari mata yang terdeteksi
+                        #     radius = int(round((w2+h2) * 0.25))
+                        #     # Tampilkan lingkaran
+                        #     frame = cv2.circle(frame, eye_center, radius, (0, 255, 0), 2)
+
+                    else :
+                        cv2.putText(frame, 'Tidak Senyum', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                        cv2.rectangle(frame, (x, y), (w + x, h + y), (0, 0,255), 2)
+
+
+        # print(faces)
 
         cv2.imshow('Frame',frame)
 
